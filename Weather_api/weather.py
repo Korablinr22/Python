04070628f7bd1792eada737dbc5bd593c6738a4c
Roman <164523311+Korablinr22@users.noreply.github.com>
2024-09-26 21:07:39 +0700

    Update weather.py

--- Weather_api/weather.py
+++ Weather_api/weather.py
@@ -5,10 +5,18 @@ import seaborn as sb
 import sqlalchemy as sql
 
 # создаем переменные для запроса
-api_key = ***
-city = "524901"
-url = "http://api.openweathermap.org/data/2.5/find?q=Petersburg&type=like&APPID=***"
+ создаем переменные для запроса
+api_key = 'cf4d1efcc9b3ee304ac0562d0359b27a'
+city = 'Biysk'
+url = f'http://api.openweathermap.org/data/2.5/find?q={city}&type=like&APPID={api_key}'
 response = req.get(url)
+
+# Проверяем статус ответа
+if response.status_code == 200:
+    print(f'Соединение установлено, код ответа: {response.status_code}')
+else:
+    print(f'Что-то пошло не так, код ошибки: {response.status_code}')
+
 print(response)
 
 # получаем json
