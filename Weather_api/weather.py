38df879e171a1e815af4593f087b627e5a4ddb5c
Roman <164523311+Korablinr22@users.noreply.github.com>
2024-09-26 20:54:20 +0700

    Update weather.py

--- Weather_api/weather.py
+++ Weather_api/weather.py
@@ -1,5 +1,17 @@
-# Устанавливаем библиотеки
+# импортируем библиотеки
 import requests as req
 import pandas as pd
 import seaborn as sb
 import sqlalchemy as sql
+
+# создаем переменные для запроса
+api_key = ***
+city = "524901"
+url = "http://api.openweathermap.org/data/2.5/find?q=Petersburg&type=like&APPID=***"
+response = req.get(url)
+print(response)
+
+# получаем json
+data = response.json()
+
+print(data)
